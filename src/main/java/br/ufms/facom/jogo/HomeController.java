b4f7e66758ce77f2731027bafe7d05ee41fef5ee
Hudson Silva Borges <hudsonsilbor@gmail.com>
2022-09-27 13:46:37 -0400

    primeiro servlet

---
+++ src/main/java/br/ufms/facom/jogo/HomeController.java
@@ -0,0 +1,33 @@
+package br.ufms.facom.jogo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeController
+ */
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HomeController() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String nome = request.getParameter("nome");
+		if (nome != null)
+			response.getWriter().append("Ola " + nome);
+		else
+			response.getWriter().append("Ola pessoas de PROGWEB");
+	}
+}
